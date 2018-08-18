3ec28db6eb319c232bab0804925bab8508774e81
pwirth <wirth@hallowelt.biz>
2018-08-18 15:07:29 +0200

    Fixed runtime error
    Change-Id: I00c015ef985cc5adad7ac9db2879fa6ba6d4990d

--- resources/ext.bluespice.pageassignments.GraphicalList.js
+++ resources/ext.bluespice.pageassignments.GraphicalList.js
@@ -1,58 +1,61 @@
-bs.util.registerNamespace( 'bs.pageassignments' );
+( function( mw, $, bs, d, undefined ){
+	bs.util.registerNamespace( 'bs.pageassignments' );
 
-bs.pageassignments.GraphicalList = function() {};
-bs.pageassignments.GraphicalList.prototype.getTitle = function() {
-	return '';
-};
+	bs.pageassignments.GraphicalList = function() {};
+	bs.pageassignments.GraphicalList.prototype.getTitle = function() {
+		return '';
+	};
 
-bs.pageassignments.GraphicalList.prototype.getActions = function() {
-	return [];
-};
+	bs.pageassignments.GraphicalList.prototype.getActions = function() {
+		return [];
+	};
 
-bs.pageassignments.GraphicalList.prototype.getBody = function() {
-	var me = this;
-	var dfd = $.Deferred();
+	bs.pageassignments.GraphicalList.prototype.getBody = function() {
+		var me = this;
+		var dfd = $.Deferred();
 
-	var htmlForSitetools = mw.config.get( 'bsgPageAssignmentsSitetools' );
+		var htmlForSitetools = mw.config.get( 'bsgPageAssignmentsSitetools' );
 
-	dfd.resolve( function() {
-		var html = '<div class="grapicallist-pageassignments-body">';
-		for( var type in htmlForSitetools ) {
-			html += '<div><span class="bs-icon-' + type + ' section">'
-				+ me.getTypeMessage( type )
-				+ '</span>';
-			for( var i = 0; i < htmlForSitetools[type].length; i++ ) {
-				html += htmlForSitetools[type][i]['html'];
+		dfd.resolve( function() {
+			var html = '<div class="grapicallist-pageassignments-body">';
+			for( var type in htmlForSitetools ) {
+				html += '<div><span class="bs-icon-' + type + ' section">'
+					+ me.getTypeMessage( type )
+					+ '</span>';
+				for( var i = 0; i < htmlForSitetools[type].length; i++ ) {
+					html += htmlForSitetools[type][i]['html'];
+				}
+				html += '</div>';
 			}
 			html += '</div>';
+
+			return html;
+		} );
+
+
+		return dfd;
+	};
+
+	bs.pageassignments.GraphicalList.prototype.getTypeMessage = function( type ) {
+		var msg = mw.message( 'bs-pageassignments-assignee-type-' + type );
+		return msg.exists() ? msg : type;
+	};
+
+	bs.pageassignments.GraphicalListFactory = function() {
+		return new bs.pageassignments.GraphicalList();
+	};
+
+
+	$( d ).on( 'click', "*[data-target]", function(e){
+		var target = $( this ).data( "target" );
+		if( target === 'graphical-list-action-preview' ){
+			$( '.grapicallist-pageassignments-body .list' ).css( 'display', 'none' );
+			$( '.grapicallist-pageassignments-body .preview' ).css( 'display', 'block' );
 		}
-		html += '</div>';
+		if( target === 'graphical-list-action-list' ){
+			$( '.grapicallist-pageassignments-body .list' ).css( 'display', 'block' );
+			$( '.grapicallist-pageassignments-body .preview' ).css( 'display', 'none' );
+		}
+	});
 
-		return html;
-	} );
-
-
-	return dfd;
-};
-
-bs.pageassignments.GraphicalList.prototype.getTypeMessage = function( type ) {
-	var msg = mw.message( 'bs-pageassignments-assignee-type-' + type );
-	return msg.exists() ? msg : type;
-};
-
-bs.pageassignments.GraphicalListFactory = function() {
-	return new bs.pageassignments.GraphicalList();
-};
-
-
-$( document ).on( 'click', "*[data-target]", function(e){
-	var target = $( this ).data( "target" );
-	if( target === 'graphical-list-action-preview' ){
-		$( '.grapicallist-pageassignments-body .list' ).css( 'display', 'none' );
-		$( '.grapicallist-pageassignments-body .preview' ).css( 'display', 'block' );
-	}
-	if( target === 'graphical-list-action-list' ){
-		$( '.grapicallist-pageassignments-body .list' ).css( 'display', 'block' );
-		$( '.grapicallist-pageassignments-body .preview' ).css( 'display', 'none' );
-	}
-});
+})( mediaWiki, jQuery, blueSpice, document );
