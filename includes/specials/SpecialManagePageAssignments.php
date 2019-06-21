71a7fd9ef959bad6a4af4329d3f92ddb00594678
Dejan Savuljesku <d.savuljesku@gmail.com>
2019-06-21 15:51:47 +0200

    Switch to new Manager SpecialPage base class
    This is done in order to integrate unified loading indicators
    
    [3.1]
    
    Requires: gerrit:518223
    
    Change-Id: Id0a37bf10bff883588c2a74366e83889e3c41a13
    ERM: #14896

--- includes/specials/SpecialManagePageAssignments.php
+++ includes/specials/SpecialManagePageAssignments.php
@@ -1,20 +1,34 @@
 <?php
 
-class SpecialManagePageAssignments extends \BlueSpice\SpecialPage {
+use BlueSpice\Special\ManagerBase;
+
+class SpecialManagePageAssignments extends ManagerBase {
 	public function __construct( $name = '', $restriction = '', $listed = true, $function = false, $file = 'default', $includable = false ) {
 		parent::__construct( 'ManagePageAssignments', 'pageassignments', $listed, $function, $file, $includable );
 	}
 
-	public function execute( $sParameter ) {
-		parent::execute( $sParameter );
-
-		$this->getOutput()->addModules( 'ext.pageassignments.manager' );
-		$aDeps = [];
-		Hooks::run( 'BSPageAssignmentsManager', [ $this, &$aDeps ] );
-		$this->getOutput()->addJsConfigVars( 'bsPageAssignmentsManagerDeps', $aDeps );
-		$this->getOutput()->addHTML(
-			Html::element( 'div', [ 'id' => 'bs-pageassignments-manager', 'class' => 'bs-manager-container' ] )
-		);
+	/**
+	 * @return string ID of the HTML element being added
+	 */
+	protected function getId() {
+		return 'bs-pageassignments-manager';
 	}
 
+	/**
+	 * @return array
+	 */
+	protected function getModules() {
+		return [
+			'ext.pageassignments.manager'
+		];
+	}
+
+	protected function getJSVars() {
+		$aDeps = [];
+		Hooks::run( 'BSPageAssignmentsManager', [ $this, &$aDeps ] );
+
+		return [
+			'bsPageAssignmentsManagerDeps' => $aDeps
+		];
+	}
 }
