9e5a9c189e302de68a693d11c7fa149fe3926708
pwirth <wirth@hallowelt.biz>
2019-07-12 12:55:29 +0200

    Fix user initialization to early
    => Needs cherry-pick to REL1_31_dev
    
    Change-Id: Ibbdbd5d3f9e999949f671d34108366d0c849aa8b

--- src/PageAssignmentsPropertyValueProvider.php
+++ src/PageAssignmentsPropertyValueProvider.php
@@ -17,18 +17,29 @@ class PageAssignmentsPropertyValueProvider extends PropertyValueProvider {
 
 	/**
 	 *
-	 * @param AssignmentFactory $assignmentFactory
+	 * @param AssignmentFactory|null $assignmentFactory
 	 */
-	public function __construct( $assignmentFactory ) {
+	public function __construct( $assignmentFactory = null ) {
 		$this->assignmentFactory = $assignmentFactory;
 	}
 
 	/**
 	 *
+	 * @param AssignmentFactory|null $factory
 	 * @return \BlueSpice\SMWConnector\IPropertyValueProvider[]
 	 */
-	public static function factory() {
-		$factory = Services::getInstance()->getService( 'BSPageAssignmentsAssignmentFactory' );
+	public static function factory( AssignmentFactory $factory = null ) {
+		// We do not create the service for injection here, because it requires
+		// an instance of LinkRenderer and will create it from the services.
+		// The LinkRenderer requires the user object to be initialized which it
+		// is not in context of the wgExtensionFunctions callbacks the registry
+		// is running in. This leads to bad issues such as the user options can
+		// not be saved anymore.
+		// Services should used with caution before everything is initialized.
+		// This needs a follow up change in the AssignmentFactory service some day,
+		// which seperates the AssignmentFactory into a raw data factory which has an
+		// assignment type renderer with all the advanced things like link
+		// rendering and such
 
 		return [ new self( $factory ) ];
 	}
@@ -78,6 +89,11 @@ class PageAssignmentsPropertyValueProvider extends PropertyValueProvider {
 			return null;
 		}
 
+		if ( !$this->assignmentFactory ) {
+			$this->assignmentFactory = Services::getInstance()->getService(
+				'BSPageAssignmentsAssignmentFactory'
+			);
+		}
 		$target = $this->assignmentFactory->newFromTargetTitle( $title );
 		if ( $target === false ) {
 			return null;
