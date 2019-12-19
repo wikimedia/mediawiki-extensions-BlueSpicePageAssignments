f1c31e3a97ed38b48ae8e71bdd25c0a61da08c30
rvogel <vogel@hallowelt.biz>
2019-12-19 16:50:14 +0100

    Add maintenance script for management
    ERM17737
    ERM14472
    
    [3.1.x]
    
    Change-Id: I63b0bc442c5b1fa9c343ad71f1d3676405c3bc7f

---
+++ maintenance/ManageAssignments.php
@@ -0,0 +1,148 @@
+<?php
+$IP = dirname( dirname( dirname( __DIR__ ) ) );
+require_once $IP . '/extensions/BlueSpiceFoundation/maintenance/BSMaintenance.php';
+
+use BlueSpice\Data\ReaderParams;
+use BlueSpice\PageAssignments\Data\Record;
+
+class ManageAssignments extends BSMaintenance {
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->addOption( 'action', 'list|set', true, true );
+		$this->addOption( 'pagelist', 'File with page titles', false, true );
+		$this->addOption( 'assignments', 'File with assignments', false, true );
+	}
+
+	public function execute() {
+		$action = $this->getOption( 'action' );
+		if ( $action === 'list' ) {
+			$this->listAssignments();
+		}
+		if ( $action === 'set' ) {
+			$this->setAssignments();
+		}
+	}
+
+	private function listAssignments() {
+		$factory = $this->getFactory();
+		$recordSet = $factory->getStore()->getReader()->read(
+			new ReaderParams( [
+				ReaderParams::PARAM_LIMIT => ReaderParams::LIMIT_INFINITE
+			] )
+		);
+		$assignments = [];
+		foreach ( $recordSet->getRecords() as $record ) {
+			$id = $record->get( Record::PAGE_ID );
+
+			$title = Title::newFromID( $id );
+			if ( $title === null ) {
+				$this->error( "Could not create Title for '$id'!" );
+				continue;
+			}
+
+			$titleText = $title->getPrefixedDBkey();
+			if ( !isset( $assignments[$titleText] ) ) {
+				$assignments[$titleText] = [];
+			}
+
+			$assignments[$titleText][] =
+				$record->get( Record::ASSIGNEE_TYPE ) . '/' . $record->get( Record::ASSIGNEE_KEY );
+		}
+
+		foreach ( $assignments as $pageName => $assignmentEntries ) {
+			$this->output( "\n$pageName" );
+			foreach ( $assignmentEntries as $assignmentEntry ) {
+				$this->output( "\t--> $assignmentEntry" );
+			}
+		}
+	}
+
+	private function setAssignments() {
+		$titles = $this->makeTitles();
+
+		foreach ( $titles as $title ) {
+			$this->output( "Processing '{$title->getPrefixedDBkey()}'..." );
+			$target = $this->getFactory()->newFromTargetTitle( $title );
+			if ( $target instanceof \BlueSpice\PageAssignments\ITarget == false ) {
+				continue;
+			}
+			$assignments = $this->makeAssignments( $title );
+			$status = $target->save( $assignments );
+			if ( $status->isOK() ) {
+				$this->output( '--> DONE.' );
+			} else {
+				$this->error( '--> ERROR: ' . $status->getMessage() );
+			}
+		}
+	}
+
+	/**
+	 *
+	 * @return \BlueSpice\PageAssignments\AssignmentFactory
+	 */
+	private function getFactory() {
+		return BlueSpice\Services::getInstance()->getService( 'BSPageAssignmentsAssignmentFactory' );
+	}
+
+	/**
+	 * @param Title $title
+	 * @return \BlueSpice\PageAssignments\IAssignment
+	 */
+	private function makeAssignments( $title ) {
+		$assignmentsDef = $this->getOption( 'assignments' );
+		$content = file_get_contents( $assignmentsDef );
+		$assignmentsList = FormatJson::decode( $content );
+		$factory = $this->getFactory();
+		$assignments = [];
+
+		foreach ( $assignmentsList as $assignmentsId ) {
+			$kvPair = explode( '/', $assignmentsId, 2 );
+			$type = $kvPair[0];
+			$key = $kvPair[1];
+			$assignment = $factory->factory( $type, $key, $title );
+			if ( $assignment instanceof \BlueSpice\PageAssignments\IAssignment === false ) {
+				$this->error(
+					"Could not create assignment for '$assignmentsId' "
+						. "on '{$title->getPrefixedDBkey()}'!"
+				);
+				continue;
+			}
+			$assignments[] = $assignment;
+		}
+
+		return $assignments;
+	}
+
+	/**
+	 * @return Title
+	 */
+	private function makeTitles() {
+		$pagelist = $this->getOption( 'pagelist' );
+		$content = file_get_contents( $pagelist );
+		$lines = explode( "\n", $content );
+		$titles = [];
+		foreach ( $lines as $line ) {
+			$trimmedLine = trim( $line );
+			if ( empty( $trimmedLine ) ) {
+				continue;
+			}
+			$title = Title::newFromText( $trimmedLine );
+			if ( $title instanceof Title === false ) {
+				$this->error( "Could not create valid title from '$trimmedLine'!" );
+			}
+			$titles[] = $title;
+		}
+		return $titles;
+	}
+
+}
+
+$maintClass = 'ManageAssignments';
+if ( defined( 'RUN_MAINTENANCE_IF_MAIN' ) ) {
+	require_once RUN_MAINTENANCE_IF_MAIN;
+} else {
+	# Make this work on versions before 1.17
+	require_once RUN_MAINTENANCE_IF_MAIN;
+}
