7b09c2acb5d6866d896f910fb4202e4255fe6d7c
Dejan Savuljesku <d.savuljesku@gmail.com>
2020-06-26 14:35:45 +0200

    Send notification on group change only if group is assigned to a page
    Still, this feels too much, since user might also get generic "group change" notification.
    Maybe wording can be improved
    
    [3.1.x]
    
    ERM 17328
    
    Change-Id: I51092f354e72017cbeb5e4be04489e49d3d55862

--- src/Hook/BSUserManagerAfterSetGroups/NotifyUsers.php
+++ src/Hook/BSUserManagerAfterSetGroups/NotifyUsers.php
@@ -2,11 +2,20 @@
 
 namespace BlueSpice\PageAssignments\Hook\BSUserManagerAfterSetGroups;
 
+use BlueSpice\Context;
+use BlueSpice\Data\Filter;
+use BlueSpice\Data\ReaderParams;
+use BlueSpice\PageAssignments\Data\Assignment\Store;
+use BlueSpice\PageAssignments\Data\Record;
 use BlueSpice\PageAssignments\Notifications\GroupsAdd;
 use BlueSpice\PageAssignments\Notifications\GroupsRemove;
 use BlueSpice\UserManager\Hook\BSUserManagerAfterSetGroups;
+use MediaWiki\MediaWikiServices;
 
 class NotifyUsers extends BSUserManagerAfterSetGroups {
+	/** @var array */
+	protected $groupsWithAssignments = [];
+
 	protected function skipProcessing() {
 		if ( empty( $this->removeGroups ) && empty( $this->addGroups ) ) {
 			return true;
@@ -15,20 +24,74 @@ class NotifyUsers extends BSUserManagerAfterSetGroups {
 	}
 
 	protected function doProcess() {
-		$notificationsManager = \BlueSpice\Services::getInstance()->getService( 'BSNotificationManager' );
+		$notificationsManager = MediaWikiServices::getInstance()
+			->getService( 'BSNotificationManager' );
 		$notifier = $notificationsManager->getNotifier();
+		$this->getGroupsWithAssignments();
+
+		if ( empty( $this->groupsWithAssignments ) ) {
+			return true;
+		}
 
 		$agent = $this->getContext()->getUser();
+		$added = $this->getAssigned(
+			array_diff( $this->addGroups, $this->excludeGroups )
+		);
+		$removed = $this->getAssigned(
+			array_diff( $this->removeGroups, $this->excludeGroups )
+		);
 
-		if ( !empty( $this->removeGroups ) ) {
-			$notification = new GroupsRemove( $agent, $this->user, $this->removeGroups );
+		if ( !empty( $removed ) ) {
+			$notification = new GroupsRemove( $agent, $this->user, $removed );
+			$notifier->notify( $notification );
 		}
-		if ( !empty( $this->addGroups ) ) {
-			$notification = new GroupsAdd( $agent, $this->user, $this->addGroups );
+		if ( !empty( $added ) ) {
+			$notification = new GroupsAdd( $agent, $this->user, $added );
+			$notifier->notify( $notification );
 		}
-
-		$notifier->notify( $notification );
 
 		return true;
+	}
+
+	/**
+	 *
+	 * @return Store
+	 */
+	private function getStore() {
+		return new Store(
+			new Context( $this->getContext(), $this->getConfig() ),
+			MediaWikiServices::getInstance()->getDBLoadBalancer()
+		);
+	}
+
+	/**
+	 * @param array $groups
+	 * @return array
+	 */
+	private function getAssigned( $groups ) {
+		return array_intersect( $groups, $this->groupsWithAssignments );
+	}
+
+	/**
+	 * Get all assignments of type group
+	 */
+	private function getGroupsWithAssignments() {
+		$recordSet = $this->getStore()->getReader()->read(
+			new ReaderParams( [ 'filter' => [
+				[
+					Filter::KEY_FIELD => Record::ASSIGNEE_TYPE,
+					Filter::KEY_VALUE => 'group',
+					Filter::KEY_TYPE => 'string',
+					Filter::KEY_COMPARISON => Filter::COMPARISON_EQUALS,
+				]
+			] ] )
+		);
+
+		/** @var Record $record */
+		foreach ( $recordSet as $record ) {
+			$this->groupsWithAssignments[] = $record->get( Record::ASSIGNEE_KEY );
+		}
+
+		$this->groupsWithAssignments = array_unique( $this->groupsWithAssignments );
 	}
 }
