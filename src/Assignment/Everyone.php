4fc2f95218c4c77a5d55275454982f3f7c65300d
Andrii Shykov <banderlog17@gmail.com>
2020-10-28 19:11:00 +0200

    Check blacklist before getting "everyone" user ids
    [3.1.x]
    
    ERM16292
    
    Change-Id: Ibb0776e591ebc36f8620b5f214c2116548bb03f7

--- src/Assignment/Everyone.php
+++ src/Assignment/Everyone.php
@@ -2,6 +2,10 @@
 namespace BlueSpice\PageAssignments\Assignment;
 
 use BlueSpice\Services;
+use BlueSpice\TargetCache\Title\Target as Target;
+use BsPageContentProvider;
+use MediaWiki\MediaWikiServices as MediaWikiServices;
+use Title;
 
 class Everyone extends \BlueSpice\PageAssignments\Assignment {
 
@@ -46,9 +50,38 @@ class Everyone extends \BlueSpice\PageAssignments\Assignment {
 		$loadBalancer = Services::getInstance()->getDBLoadBalancer();
 		$res = $loadBalancer->getConnection( DB_REPLICA )->select(
 			'user',
-			'user_id'
+			[ 'user_id', 'user_name' ]
 		);
+
+		$blacklistedUsers = [];
+		$pageName = 'PageAssignments-everyone-blacklist';
+		$blacklistedUsersTitle = Title::makeTitle( NS_MEDIAWIKI, $pageName );
+
+		if ( $blacklistedUsersTitle->exists() ) {
+
+			$titleCache = MediaWikiServices::getInstance()->getService( 'BSTargetCacheTitle' );
+			$target = new Target( $blacklistedUsersTitle );
+			$cacheHandler = $titleCache->getHandler( 'pageassignments-everyone-blacklist', $target );
+			$blacklistContent = $cacheHandler->get();
+			if ( $blacklistContent === false ) {
+				$blacklistContent = trim(
+					BsPageContentProvider::getInstance()->getContentFromTitle( $blacklistedUsersTitle )
+				);
+				$cacheHandler->set( $blacklistContent );
+			}
+			if ( !empty( $blacklistContent ) ) {
+				$blacklistedUsers = explode( "\n", $blacklistContent );
+				foreach ( $blacklistedUsers as $k => $v ) {
+					$blacklistedUsers[$k] = trim( $v );
+				}
+			}
+
+		}
+
 		foreach ( $res as $row ) {
+			if ( in_array( $row->user_name, $blacklistedUsers ) ) {
+				continue;
+			}
 			$allowed = $this->getTitle()->userCan(
 				'pageassignable',
 				\User::newFromId( (int)$row->user_id )
@@ -58,7 +91,6 @@ class Everyone extends \BlueSpice\PageAssignments\Assignment {
 			}
 			static::$userIdCache[] = (int)$row->user_id;
 		}
-
 		return static::$userIdCache;
 	}
 
