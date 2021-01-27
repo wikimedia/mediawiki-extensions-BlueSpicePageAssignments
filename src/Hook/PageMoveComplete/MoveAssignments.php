53b6efe6769a606d447eb96f7b2f4e1fb8186063
pwirth <wirth@hallowelt.biz>
2021-01-27 09:57:30 +0100

    Replace deprecated hook TitleMoveComplete ...
    ...with hook PageMoveComplete
    
    ERM:19636
    [3.3]
    
    Change-Id: I1292639420dfb7ae472bfdc0e696f7c9ee4cf51d

---
+++ src/Hook/PageMoveComplete/MoveAssignments.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BlueSpice\PageAssignments\Hook\PageMoveComplete;
+
+use BlueSpice\Hook\PageMoveComplete;
+use Title;
+
+/**
+ * Adapts assignments in case of article move.
+ */
+class MoveAssignments extends PageMoveComplete {
+
+	/**
+	 *
+	 * @return bool
+	 */
+	protected function skipProcessing() {
+		return Title::newFromLinkTarget( $this->new )->equals( $this->old );
+	}
+
+	/**
+	 *
+	 * @return bool
+	 */
+	protected function doProcess() {
+		$this->getServices()->getDBLoadBalancer( DB_MASTER )->update(
+			'bs_pageassignments',
+			[
+				'pa_page_id' => Title::newFromLinkTarget( $this->new )->getArticleID()
+			],
+			[
+				'pa_page_id' => Title::newFromLinkTarget( $this->old )->getArticleID()
+			]
+		);
+		return true;
+	}
+
+}
