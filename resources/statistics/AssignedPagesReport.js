44fee3b01379b8efdd15bdde61a8e5f36f205e0f
d.savuljesku <savuljesku@hallowelt.com>
2021-09-17 08:48:17 +0200

    Implement new Statistics snapshot providers
    Requires I999788b1dfef1ac20cf6df6a265b2983ab571e50
    
    [3.2.x]
    
    ERM 25753
    
    Change-Id: Iea0bcaf145d215e3fddb27a42db3c15409ca71ee

---
+++ resources/statistics/AssignedPagesReport.js
@@ -0,0 +1,30 @@
+(function ( mw, $, bs) {
+	bs.util.registerNamespace( 'bs.pageAssignments.report' );
+
+	bs.pageAssignments.report.AssignedPagesReport = function ( cfg ) {
+		bs.pageAssignments.report.AssignedPagesReport.parent.call( this, cfg );
+	};
+
+	OO.inheritClass( bs.pageAssignments.report.AssignedPagesReport, bs.aggregatedStatistics.report.ReportBase );
+
+	bs.pageAssignments.report.AssignedPagesReport.static.label = mw.message( 'bs-pageassignments-statistics-report-assigned-pages' ).text();
+
+	bs.pageAssignments.report.AssignedPagesReport.prototype.getFilters = function () {
+		return [
+			new bs.aggregatedStatistics.filter.IntervalFilter(),
+			new bs.aggregatedStatistics.filter.NamespaceCategoryFilter( { onlyContentNamespaces: true } )
+		];
+	};
+
+	bs.pageAssignments.report.AssignedPagesReport.prototype.getChart = function () {
+		return new bs.aggregatedStatistics.charts.Groupchart();
+	};
+
+	bs.pageAssignments.report.AssignedPagesReport.prototype.getAxisLabels = function () {
+		return {
+			assigned: mw.message( "bs-pageassignments-statistics-report-assigned-pages-axis-assigned" ).text(),
+			unassigned: mw.message( "bs-pageassignments-statistics-report-assigned-pages-axis-unassigned" ).text()
+		};
+	};
+
+} )( mediaWiki, jQuery , blueSpice);
