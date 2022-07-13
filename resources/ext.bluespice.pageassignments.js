2b84e29e4cf7cbddb206a0e7aa9818da4a5372c6
rvogel <vogel@hallowelt.biz>
2022-07-13 13:03:34 +0200

    Load all dependencies of "Set page assignments" dialog
    It would crash otherwise, if `OOJSPlus` RL modules are not loaded
    properly (`OOJSPlus.ui.dialog.BookletDialog` was not available).
    
    ERM29194
    
    [4.1.x]
    
    Change-Id: I093d48426a06e96d05493286ea0c61a338a85e94
    (cherry picked from commit a7cec06461b051ac370d02430099bbc2d97a62fe)
    (cherry picked from commit b483cda38a152f7484986bcf12763fe62c3643ab)
    (cherry picked from commit fc80912fa25ed036baa1d9e28ecfb5d7284b8996)

--- resources/ext.bluespice.pageassignments.js
+++ resources/ext.bluespice.pageassignments.js
@@ -1,46 +1,44 @@
 (function( mw, $, d, undefined ){
-	blueSpice.util.registerNamespace( 'bs.pageassignments.ui' );
+	function _showDialog() {
+		var dfd = $.Deferred();
 
-	$(d).on( 'click', '#ca-pageassignments a, a#ca-pageassignments', function( e ) {
-		e.preventDefault();
+		var api = new mw.Api();
+		api.postWithToken( 'csrf', {
+			'action': 'bs-pageassignment-tasks',
+			'formatversion': 2,
+			'task': 'getForPage',
+			'taskData': JSON.stringify( {
+				pageId: mw.config.get( 'wgArticleId' )
+			} )
+		} ).done( function( response, xhr ){
+			if ( response.success ) {
+				dfd.resolve( [
+					new bs.pageassignments.ui.AssignmentsPage( {
+						data: {
+							page: mw.config.get( 'wgArticleId' ),
+							assignments: response.payload
+						}
+					} )
+				] );
+			} else {
+				dfd.reject();
+			}
+		} ).fail( function( e ) {
+			dfd.reject();
+		} );
 
 		var dialog = new OOJSPlus.ui.dialog.BookletDialog( {
 			id: 'bs-pageassignments-set',
 			pages: function() {
-				var dfd = $.Deferred();
-				mw.loader.using( "ext.bluespice.pageassignments.dialog.pages", function() {
-					var api = new mw.Api();
-					api.postWithToken( 'csrf', {
-						'action': 'bs-pageassignment-tasks',
-						'formatversion': 2,
-						'task': 'getForPage',
-						'taskData': JSON.stringify( {
-							pageId: mw.config.get( 'wgArticleId' )
-						} )
-					} ).done( function( response, xhr ){
-						if ( response.success ) {
-							dfd.resolve( [
-								new bs.pageassignments.ui.AssignmentsPage( {
-									data: {
-										page: mw.config.get( 'wgArticleId' ),
-										assignments: response.payload
-									}
-								} )
-							] );
-						} else {
-							dfd.reject();
-						}
-					} ).fail( function( e ) {
-						dfd.reject();
-					} );
-
-				}, function( e ) {
-					dfd.reject( e );
-				} );
 				return dfd.promise();
 			}
 		} );
 
 		dialog.show();
+	}
+
+	$(d).on( 'click', '#ca-pageassignments a, a#ca-pageassignments', function( e ) {
+		e.preventDefault();
+		mw.loader.using( 'ext.bluespice.pageassignments.dialog.pages' ).done( _showDialog );
 	} );
 })( mediaWiki, jQuery, document );
