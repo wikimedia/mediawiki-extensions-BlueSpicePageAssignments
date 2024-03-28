6dce12c1361a1ee784ecddfaff6583cec271f8ab
d.savuljesku <d.savuljesku@gmail.com>
2024-03-28 11:09:14 +0100

    Assignable store performance
    [4.4.x]
    
    ERM36506
    
    Change-Id: Ic73cf218e2ecf3fa9dd8e01dd913d39e7865e71a

--- src/Data/Assignable/User/PrimaryDataProvider.php
+++ src/Data/Assignable/User/PrimaryDataProvider.php
@@ -2,27 +2,31 @@
 
 namespace BlueSpice\PageAssignments\Data\Assignable\User;
 
-use BlueSpice\Data\User\Record;
-use BlueSpice\PageAssignments\IAssignment;
+use BlueSpice\PageAssignments\Data\Assignable\User\ReaderParams as UserReaderParams;
+use BlueSpice\PageAssignments\Data\Record;
 use GlobalVarConfig;
 use MediaWiki\MediaWikiServices;
 use MWStake\MediaWiki\Component\DataStore\ReaderParams;
 use MWStake\MediaWiki\Component\DataStore\Schema;
 use Title;
+use User;
 use Wikimedia\Rdbms\IDatabase;
 
 class PrimaryDataProvider extends \MWStake\MediaWiki\Component\CommonWebAPIs\Data\UserQueryStore\PrimaryDataProvider {
-
-	/**
-	 * @var null | ReaderParams
-	 */
-	private $params = null;
 
 	/**
 	 *
 	 * @var Title
 	 */
 	protected $title = null;
+
+	/**
+	 * @var array
+	 */
+	private $groupTestUsers = [];
+
+	/** @var array */
+	private $addedUsers = [];
 
 	/**
 	 * @param IDatabase $db
@@ -37,12 +41,44 @@ class PrimaryDataProvider extends \MWStake\MediaWiki\Component\CommonWebAPIs\Dat
 
 	/**
 	 * @param ReaderParams $params
-	 *
 	 * @return \MWStake\MediaWiki\Component\DataStore\Record[]
 	 */
 	public function makeData( $params ) {
-		$this->params = $params;
-		return parent::makeData( $params );
+		return parent::makeData( new UserReaderParams( $params ) );
+	}
+
+	/**
+	 * @param ReaderParams $params
+	 * @return array
+	 */
+	protected function makePreFilterConds( ReaderParams $params ) {
+		$conds = parent::makePreFilterConds( $params );
+		$assignableGroups = $this->getAssignableGroups();
+		if ( in_array( 'user', $assignableGroups ) ) {
+			// No need to change anything, every user is allowed
+			return $conds;
+		}
+		$conds[] = 'ug_group IN (' . $this->db->makeList( $assignableGroups ) . ')';
+
+		return $conds;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function getJoinConds( ReaderParams $params ) {
+		return parent::getJoinConds( $params ) + [
+			'user_groups' => [
+				'LEFT OUTER JOIN', [ 'user_id = ug_user' ]
+			]
+		];
+	}
+
+	/**
+	 * @return string[]
+	 */
+	protected function getTableNames() {
+		return array_merge( parent::getTableNames(), [ 'user_groups' ] );
 	}
 
 	/**
@@ -50,43 +86,62 @@ class PrimaryDataProvider extends \MWStake\MediaWiki\Component\CommonWebAPIs\Dat
 	 * @param \stdClass $row
 	 */
 	protected function appendRowToData( $row ) {
-		if ( $this->params->getQuery() !== '' ) {
-			$bApply = \BsStringHelper::filter(
-				\BsStringHelper::FILTER_CONTAINS,
-				$row->{Record::USER_NAME},
-				$this->params->getQuery()
-			) || \BsStringHelper::filter(
-				\BsStringHelper::FILTER_CONTAINS,
-				$row->{Record::USER_REAL_NAME},
-				$this->params->getQuery()
-			);
-			if ( !$bApply ) {
-				return;
-			}
-		}
-
-		$services = MediaWikiServices::getInstance();
-		$user = $services->getUserFactory()->newFromId( $row->{Record::ID} );
-		if ( !$user ) {
+		$username = $row->user_name;
+		if ( isset( $this->addedUsers[$username] ) ) {
 			return;
 		}
+		$this->addedUsers[$username] = true;
+		$this->data[] = new Record( (object)[
+			Record::ASSIGNEE_KEY => $username,
+			Record::ASSIGNEE_TYPE => 'user',
+			Record::PAGE_ID => $this->title->getArticleID(),
+			Record::TEXT => $username
+		] );
+	}
 
-		if ( !$services->getPermissionManager()
-			->userCan( 'pageassignable', $user, $this->title )
-		) {
-			return;
+	/**
+	 * @return array
+	 */
+	private function getAssignableGroups(): array {
+		$groups = [];
+
+		// Unfortunately, due to infra of PA extension, cannot inject
+		$userFactory = MediaWikiServices::getInstance()->getUserFactory();
+		$pm = MediaWikiServices::getInstance()->getPermissionManager();
+		$userNoGroup = User::newSystemUser( 'DummyCheckAssignable' );
+		// Check if user with no groups can be assigned, if yes, do not check for other groups
+		if ( $pm->userCan( 'pageassignable', $userNoGroup, $this->title ) ) {
+			$groups[] = 'user';
+			return $groups;
 		}
-		$assignmentFactory = $services->getService( 'BSPageAssignmentsAssignmentFactory' );
-		$assignment = $assignmentFactory->factory(
-			'user',
-			$row->{Record::USER_NAME},
-			$this->title
+
+		// If not every user can be assigned, check which groups can
+		$res = $this->db->select(
+			'user_groups',
+			[ 'DISTINCT( ug_group ) user_group', 'MIN( ug_user ) user' ],
+			[],
+			__METHOD__,
+			[ 'GROUP BY' => 'ug_group' ]
 		);
-		if ( !$assignment instanceof IAssignment ) {
-			// :(
-			return;
+
+		foreach ( $res as $row ) {
+			$group = $row->user_group;
+			$user = $row->user;
+			if ( !isset( $this->groupTestUsers[$group] ) ) {
+				$userObject = $userFactory->newFromId( $user );
+				$this->groupTestUsers[$group] = $userObject;
+			}
+			$userObject = $this->groupTestUsers[$group];
+			if ( !$userObject ) {
+				continue;
+			}
+			$allowed = $pm->userCan( 'pageassignable', $userObject, $this->title );
+			if ( !$allowed ) {
+				continue;
+			}
+			$groups[] = $group;
 		}
 
-		$this->data[] = $assignment->getRecord();
+		return $groups;
 	}
 }
---
+++ src/Data/Assignable/User/SecondaryDataProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace BlueSpice\PageAssignments\Data\Assignable\User;
+
+use BlueSpice\PageAssignments\Data\Record;
+use BlueSpice\PageAssignments\IAssignment;
+use MediaWiki\MediaWikiServices;
+use MWStake\MediaWiki\Component\DataStore\ISecondaryDataProvider;
+
+class SecondaryDataProvider implements ISecondaryDataProvider {
+	/**
+	 * @param array $dataSets
+	 * @return array
+	 */
+	public function extend( $dataSets ) {
+		$assignmentFactory = MediaWikiServices::getInstance()->getService( 'BSPageAssignmentsAssignmentFactory' );
+		$titleFactory = MediaWikiServices::getInstance()->getTitleFactory();
+		$userFactory = MediaWikiServices::getInstance()->getUserFactory();
+
+		$processed = [];
+		foreach ( $dataSets as $record ) {
+			$user = $userFactory->newFromName( $record->get( Record::TEXT ) );
+			if ( !$user ) {
+				continue;
+			}
+
+			$assignment = $assignmentFactory->factory(
+				'user',
+				$user->getName(),
+				$titleFactory->newFromID( $record->get( Record::PAGE_ID ) )
+			);
+			if ( !$assignment instanceof IAssignment ) {
+				// :(
+				continue;
+			}
+
+			$processed[] = $assignment->getRecord();
+		}
+
+		return $processed;
+	}
+}
