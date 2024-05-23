63f740f8483a2b5726e5323b1e92e949b9899e7f
Mschlindwein <schlindwein@hallowelt.com>
2024-05-23 10:34:53 +0200

    Add overview action for bookshelf
    ERM37316
    
    [4.5.x]
    
    Depends-On: I4612bdca2a2eea5f11bb9612d6d859bb56801852
    
    Change-Id: Ib07e451e77b2919dc673050a49d4a4b21b9ba878

---
+++ src/BookOverviewActions/Assignments.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace BlueSpice\PageAssignments\BookOverviewActions;
+
+use BlueSpice\Bookshelf\IBooksOverviewAction;
+use Message;
+use Title;
+
+class Assignments implements IBooksOverviewAction {
+
+        /**
+         * @var Title
+         */
+	private $book = null;
+
+	/**
+	 * @var string
+	 */
+	private $displayTitle = '';
+
+	/**
+	 * @param Title $book
+	 * @param string $displayTitle
+	 */
+	public function __construct( Title $book, string $displayTitle ) {
+		$this->book = $book;
+		$this->displayTitle = $displayTitle;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName(): string {
+		return 'assignments';
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getPosition(): int {
+		return 80;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getClasses(): array {
+		return [ 'pageassignments-book-overview' ];
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getIconClasses(): array {
+		return [ 'bi-file-earmark-person' ];
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getText(): Message {
+		return new Message( 'bs-pageassignments-books-overview-page-book-action-assignment-text' );
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getTitle(): Message {
+		$titleText = $this->book->getPrefixedText();
+		if ( $this->displayTitle !== '' ) {
+			$titleText = $this->displayTitle;
+		}
+		return new Message(
+			'bs-pageassignments-books-overview-page-book-action-assignment-title',
+			[ $titleText ]
+		);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getHref(): string {
+		return '';
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getRequiredPermission(): string {
+		return 'pageassignable';
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getRLModules(): array {
+		return [ 'ext.pageassignments.bookshelf.overviewAction' ];
+	}
+}
