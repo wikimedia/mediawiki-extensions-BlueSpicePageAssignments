9d35fa4bc9152dd32f83e41bccc5746e5403eef1
Mschlindwein <schlindwein@hallowelt.com>
2024-05-23 10:34:53 +0200

    Add overview action for bookshelf
    ERM37316
    
    [4.5.x]
    
    Depends-On: I4612bdca2a2eea5f11bb9612d6d859bb56801852
    
    Change-Id: Ib07e451e77b2919dc673050a49d4a4b21b9ba878
    (cherry picked from commit 63f740f8483a2b5726e5323b1e92e949b9899e7f)

---
+++ src/HookHandler/BSBookshelf.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace BlueSpice\PageAssignments\HookHandler;
+
+use BlueSpice\Bookshelf\Hook\BSBookshelfBooksOverviewBeforeSetBookActions;
+use BlueSpice\PageAssignments\BookOverviewActions\Assignments;
+use Title;
+
+class BSBookshelf implements BSBookshelfBooksOverviewBeforeSetBookActions {
+
+	/**
+	 * @param array &$actions
+	 * @param Title $book
+	 * @param string $displayTitle
+	 * @return void
+	 */
+	public function onBSBookshelfBooksOverviewBeforeSetBookActions(
+		array &$actions, Title $book, string $displayTitle
+	): void {
+		$actions['assignments'] = new Assignments( $book, $displayTitle );
+	}
+}
