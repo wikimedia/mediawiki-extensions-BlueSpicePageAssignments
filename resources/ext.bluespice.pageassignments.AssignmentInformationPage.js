44bfefb37befc15280ac432afff5e54c596ebf92
hslater <hamish.slater@hallowelt.com>
2024-09-26 15:07:48 +1200

    Convert Page information Assignments to OOJS
    Cleanup flyout
    
    ERM38823
    
    [REL1_39, REL1_39-4.5.x, master]
    
    Change-Id: Iea26ceb03d3139bfcd853af76be8b0b3b57a0776

--- resources/ext.bluespice.pageassignments.AssignmentInformationPage.js
+++ resources/ext.bluespice.pageassignments.AssignmentInformationPage.js
@@ -1,4 +1,4 @@
-(function( mw, $, bs ) {
+( ( mw, bs ) => {
 	bs.util.registerNamespace( 'bs.pageassignments.info' );
 
 	bs.pageassignments.info.AssignmentsInformationPage = function AssignmentsInformationPage( name, config ) {
@@ -6,7 +6,7 @@
 		bs.pageassignments.info.AssignmentsInformationPage.super.call( this, name, config );
 	};
 
-	OO.inheritClass( bs.pageassignments.info.AssignmentsInformationPage, StandardDialogs.ui.BasePage );
+	OO.inheritClass( bs.pageassignments.info.AssignmentsInformationPage, StandardDialogs.ui.BasePage ); // eslint-disable-line no-undef
 
 	bs.pageassignments.info.AssignmentsInformationPage.prototype.setupOutlineItem = function () {
 		bs.pageassignments.info.AssignmentsInformationPage.super.prototype.setupOutlineItem.apply( this, arguments );
@@ -20,33 +20,32 @@
 		return;
 	};
 
-	bs.pageassignments.info.AssignmentsInformationPage.prototype.onInfoPanelSelect = function () {
-		var me = this;
-		if ( me.assignmentGrid === null ){
-			mw.loader.using( 'ext.bluespice.extjs' ).done( function () {
-				Ext.onReady( function( ) {
-					me.assignmentGrid = Ext.create( 'BS.PageAssignments.flyout.grid.AssigneesPanel', {
-						title: false,
-						renderTo: me.$element[0],
-						width: me.$element.width(),
-						height: me.$element.height()
-					});
-				}, me );
-			});
+	bs.pageassignments.info.AssignmentsInformationPage.prototype.onInfoPanelSelect = async function () {
+		if ( !this.assignmentGrid ) {
+			await mw.loader.using( [ 'ext.oOJSPlus.data', 'oojs-ui.styles.icons-user' ] );
+			const data = await bs.api.tasks.exec( 'pageassignment', 'getForPage', { pageId: mw.config.get( 'wgArticleId' ) } );
+
+			this.assignmentGrid = new OOJSPlus.ui.data.GridWidget( {
+				style: 'differentiate-rows',
+				columns: {
+					pa_assignee_key: { // eslint-disable-line camelcase
+						headerText: mw.message( 'bs-pageassignments-column-assignedto' ).text(),
+						type: 'user',
+						showImage: true
+					},
+					pa_assignee_type: { // eslint-disable-line camelcase
+						headerText: mw.message( 'bs-pageassignments-column-assignee-type' ).text(),
+						type: 'text'
+
+					}
+				},
+				data: data.payload
+			} );
+
+			this.$element.append( this.assignmentGrid.$element );
 		}
 	};
 
-	bs.pageassignments.info.AssignmentsInformationPage.prototype.getData = function () {
+	registryPageInformation.register( 'assignment_infos', bs.pageassignments.info.AssignmentsInformationPage ); // eslint-disable-line no-undef
 
-		var dfd = new $.Deferred();
-		mw.loader.using( 'ext.bluespice.extjs' ).done( function () {
-			Ext.require( 'BS.PageAssignments.flyout.grid.AssigneesPanel', function() {
-				dfd.resolve();
-			});
-		});
-		return dfd.promise();
-	};
-
-	registryPageInformation.register( 'assignment_infos', bs.pageassignments.info.AssignmentsInformationPage );
-
-})( mediaWiki, jQuery, blueSpice );
+} )( mediaWiki, blueSpice );
