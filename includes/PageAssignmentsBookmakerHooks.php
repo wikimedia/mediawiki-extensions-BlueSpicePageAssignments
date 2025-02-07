e9963cf07bbd54d2f7a79613e6d9c4a8a276bd21
d.savuljesku <d.savuljesku@gmail.com>
2025-02-07 09:59:46 +0100

    Remove integration into BlueSpiceUEModulePDF
    [5.0]
    
    ERM38759
    
    Change-Id: I85c0c11686add3127f2af3ad2ff9e10850d48c1d

--- includes/PageAssignmentsBookmakerHooks.php
+++ includes/PageAssignmentsBookmakerHooks.php
@@ -6,46 +6,6 @@ use MediaWiki\Title\Title;
 class PageAssignmentsBookmakerHooks {
 
 	/**
-	 * Adds information about assignments to PDF export
-	 * @param Title $oTitle
-	 * @param DOMDocument $oPageDOM
-	 * @param array &$aParams
-	 * @param DOMXPath $oDOMXPath
-	 * @param array &$aMeta
-	 * @return bool
-	 */
-	public static function onBSUEModulePDFcollectMetaData( $oTitle, $oPageDOM,
-		&$aParams, $oDOMXPath, &$aMeta ) {
-		$aMeta['assigned_users'] = '';
-		$aMeta['assigned_groups'] = '';
-
-		$aAssignedUserNames = [];
-		$aAssignedGroupNames = [];
-
-		$target = static::getFactory()->newFromTargetTitle( $oTitle );
-		if ( $target instanceof \BlueSpice\PageAssignments\ITarget === false ) {
-			return true;
-		}
-
-		foreach ( $target->getAssignments() as $assignment ) {
-			if ( $assignment->getType() === 'user' ) {
-				$aAssignedUserNames[] = $assignment->getText();
-			}
-			if ( $assignment->getType() === 'group' ) {
-				$aAssignedGroupNames[] = $assignment->getText();
-			}
-		}
-		if ( !empty( $aAssignedUserNames ) ) {
-			$aMeta['assigned_users'] = implode( ', ', $aAssignedUserNames );
-		}
-		if ( !empty( $aAssignedGroupNames ) ) {
-			$aMeta['assigned_groups'] = implode( ', ', $aAssignedGroupNames );
-		}
-
-		return true;
-	}
-
-	/**
 	 * Adds information about assignments to the Bookshelf BookManager grid
 	 * @param Title $oBookTitle
 	 * @param stdClass $oBookRow
